2d79730f51bd201ce41386c3712c6f6102f67ac8
funnykoss <noanybody26@gmail.com>
2021-05-21 20:35:35 +0300

    6-fix

--- js/task-06.js
+++ js/task-06.js
@@ -2,18 +2,22 @@ const inputEl = document.querySelector('#validation-input');
 const resetValidationСlasses = event =>
   event.currentTarget.classList.remove('invalid', 'valid');
 
+
 const setValidationСlasses = event => {
   const el = event.currentTarget;
   const inputValidLength = Number(el.dataset.length);
 
-  if (el.value.length !== inputValidLength) {
-    el.classList.add('invalid');
+  if (el.value.length === inputValidLength) {
+    el.classList.add('valid');
     return;
-  }
-
- el.classList.add('valid');
-};
-
+  } else if (el.value.length === 0) {
+el.classList.remove('invalid');
+    return;
+  } 
+    el.classList.add('invalid');
+  };
+  
 inputEl.addEventListener('focus', resetValidationСlasses);
 
 inputEl.addEventListener('blur', setValidationСlasses);
+
